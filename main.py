57abe2dbce0b25c2728efbc3e15bdab3031bf8f8
KamilR <kamil.rajewski1@gmail.com>
2023-01-30 23:55:15 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Hello": "Welcome to my API"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your post"}
+
+
+@app.post("/createposts")
+def create_posts():
+    return {"message": "succesfully created posts"}
