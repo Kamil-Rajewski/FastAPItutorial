d363b200610e0686628bf8ffa0f54e5b6cfe1ba8
KamilR <kamil.rajewski1@gmail.com>
2023-02-06 22:45:33 +0100

    updated ORM

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -8,9 +8,20 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
-
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class Post(BaseModel):
@@ -52,6 +63,10 @@ def find_index_post(id):
 @app.get("/")
 def root():
     return {"Hello": "he to API"}
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session=Depends(get_db)):
+    return {"status": "Success"}
 
 
 @app.get("/posts")
