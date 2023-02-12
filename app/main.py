26b58e450e4fcbba91b622177baf4b9e42ec9aeb
KamilR <kamil.rajewski1@gmail.com>
2023-02-12 12:37:19 +0100

    added userout in

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
@@ -52,7 +52,7 @@ def root():
     return {"Hello": "he to API"}
 
 
-@app.get("/posts")
+@app.get("/posts", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""Select * From posts """)
     # posts = cursor.fetchall()
@@ -74,7 +74,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 
-@app.get("/posts/{id}")
+@app.get("/posts/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute(""" Select * From posts where id = %s returning *""", (str(id),))
     # post = cursor.fetchone()
@@ -101,7 +101,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}")
+@app.put("/posts/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostUpdate, db: Session = Depends(get_db)):
     # cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s
     # WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id), ))
@@ -116,3 +116,12 @@ def update_post(id: int, updated_post: schemas.PostUpdate, db: Session = Depends
         post_query.update(updated_post.dict(), synchronize_session=False)
     db.commit()
     return post_query.first()
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
