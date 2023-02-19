88441a3039214acc6df491e2fe31a7823d503a75
KamilR <kamil.rajewski1@gmail.com>
2023-02-19 21:34:29 +0100

    env variables

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import post, user, auth
+from .config import Settings
 
 
 models.Base.metadata.create_all(bind=engine, )
 app = FastAPI()
-
 
 app.include_router(post.router)
 app.include_router(user.router)
