4b2411f9a031c3718c621cde0f9d866b63220cd8
KamilR <kamil.rajewski1@gmail.com>
2023-02-22 23:38:44 +0100

    added voting

--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
+from pydantic.types import conint
+
 
 class PostBase(BaseModel):
     title: str
@@ -53,4 +55,9 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)
 
