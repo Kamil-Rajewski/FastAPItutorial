26ed05eab72d402eeee39730b98b7134fcdcd0cd
KamilR <kamil.rajewski1@gmail.com>
2023-02-24 17:41:06 +0100

    add alembic

--- app/models.py
+++ app/models.py
@@ -26,7 +26,7 @@ class User(Base):
     password = Column(String, nullable=False, unique=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text("now()"))
-    phone_number = Column(String)
+
 
 class Vote(Base):
     __tablename__ = "votes"
